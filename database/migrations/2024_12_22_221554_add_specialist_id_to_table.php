5332937debcdbde296ccbff4c7e9efcba86835d2
eslamshawky <eshawky467@gmail.com>
2024-12-23 00:16:47 +0200

    spec_id_2

--- database/migrations/2024_12_22_221554_add_specialist_id_to_table.php
+++ database/migrations/2024_12_22_221554_add_specialist_id_to_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('specialists', function (Blueprint $table) {
-            $table->text('device_token')->nullable();
+            $table->bigInteger('specialist_id')->nullable();
         });
     }
 
