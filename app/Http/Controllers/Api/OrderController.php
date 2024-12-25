2cdd57aa931cb4b98067bb7c1bc7f8412a917414
eslamshawky <eshawky467@gmail.com>
2024-12-25 04:15:39 +0200

    final_get_orders_2

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -14,13 +14,13 @@ class OrderController extends Controller
     public function orders(Request $request)
     {
         if ($request->status == 'pending') {
-            $orders = Order::where('user_id',Auth::id())->where('status', 'pending')->with('specialist')->paginate(30);
+            $orders = Order::where('user_id', Auth::id())->where('status', 'pending')->with('specialist')->paginate(30);
         }
         if ($request->status == 'schedule') {
-            $orders = Order::where('user_id',Auth::id())->where('status', 'schedule')->with('specialist')->paginate(30);
+            $orders = Order::where('user_id', Auth::id())->where('status', 'schedule')->with('specialist')->paginate(30);
         }
         if ($request->status == 'finished') {
-            $orders = Order::where('user_id',Auth::id())->where('status', 'finished')->with('specialist')->paginate(30);
+            $orders = Order::where('user_id', Auth::id())->where('status', 'finished')->with('specialist')->paginate(30);
         }
         return response()->json([
             'message' => 'success',
@@ -30,11 +30,11 @@ class OrderController extends Controller
     public function normal_orders(Request $request)
     {
         if ($request->status == 'pending') {
-            $orders = OrderNormal::with(['ordernormal.specialist'])->where('user_id',Auth::id())->
+            $orders = OrderNormal::with(['ordernormal.specialist'])->where('user_id', Auth::id())
                 ->where('status', 'pending')
                 ->paginate(30);
         } else {
-            $orders = OrderNormal::where('user_id',Auth::id())->with(['ordernormal.specialist'])
+            $orders = OrderNormal::where('user_id', Auth::id())->with(['ordernormal.specialist'])
                 ->where('status', 'finished')
                 ->paginate(30);
         }
@@ -46,13 +46,13 @@ class OrderController extends Controller
     public function  orderservices(Request $request)
     {
         if ($request->status == 'finished') {
-            $orders = OrderService::where('user_id',Auth::id())->with('specialist')->where('status', 'finished')->paginate(30);
+            $orders = OrderService::where('user_id', Auth::id())->with('specialist')->where('status', 'finished')->paginate(30);
         }
         if ($request->status == 'pending') {
-            $orders = OrderService::where('user_id',Auth::id())->with('specialist')->where('status', 'pending')->paginate(30);
+            $orders = OrderService::where('user_id', Auth::id())->with('specialist')->where('status', 'pending')->paginate(30);
         }
         if ($request->status == 'cancelled') {
-            $orders = OrderService::where('user_id',Auth::id())->with('specialist')->where('status', 'cancelled')->paginate(30);
+            $orders = OrderService::where('user_id', Auth::id())->with('specialist')->where('status', 'cancelled')->paginate(30);
         }
         return response()->json([
             'message' => 'success',
