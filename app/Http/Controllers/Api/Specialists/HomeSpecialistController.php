ca375f7c7bf3ac76fd0b528d12866bb57f71c24d
eslamshawky <eshawky467@gmail.com>
2025-01-12 15:51:31 +0200

    fix_conflicts_stage_98

--- app/Http/Controllers/Api/Specialists/HomeSpecialistController.php
+++ app/Http/Controllers/Api/Specialists/HomeSpecialistController.php
@@ -16,45 +16,44 @@ use Illuminate\Support\Facades\Validator;
 
 class HomeSpecialistController extends Controller
 {
-    //
     public function getdata()
     {
-        $data = OrderService::where('status', 'active')->with(
-            [
-                'service_special' => function ($q) {
-                    $q->select('id', 'name_' . app()->getLocale() . ' as name');
-                }
-            ]
-        )->get();
-        $data->map(function ($data) {
+        $data = OrderService::where('status', 'active')->with([
+            'service_special' => function ($q) {
+                $q->select('id', 'name_' . app()->getLocale() . ' as name');
+            }
+        ])->get();
 
+        $data->map(function ($data) {
             $data['files'] = OrderFile::where('order_id', $data['id'])->get();
         });
-        return Response::json(array(
+
+        return Response::json([
             'status' => 200,
             'message' => 'true',
             'data' => $data,
-        ));
+        ]);
     }
+
     public function get_data_by_id(Request $request)
     {
-        $data = OrderService::where('id', $request->id)->with(
-            [
-                'service_special' => function ($q) {
-                    $q->select('id', 'name_' . app()->getLocale() . ' as name');
-                }
-            ]
-        )->get();
-        $data->map(function ($data) {
+        $data = OrderService::where('id', $request->id)->with([
+            'service_special' => function ($q) {
+                $q->select('id', 'name_' . app()->getLocale() . ' as name');
+            }
+        ])->get();
 
+        $data->map(function ($data) {
             $data['files'] = OrderFile::where('order_id', $data['id'])->get();
         });
-        return Response::json(array(
+
+        return Response::json([
             'status' => 200,
             'message' => 'true',
             'data' => $data,
-        ));
+        ]);
     }
+
     public function add_negotation(Request $request)
     {
         $validator = Validator::make($request->all(), [
@@ -66,12 +65,12 @@ class HomeSpecialistController extends Controller
             'status' => 'nullable|string|max:255',
         ], [
             'order_id.required' => trans('auth.email.register'),
-
         ]);
+
         if ($validator->fails()) {
             return response()->json(['message' => $validator->errors()->first()], 422);
         }
-        // Create the order using the validated data
+
         Negotation::create([
             'order_id' => $request->order_id,
             'user_id' => $request->user_id,
@@ -80,91 +79,167 @@ class HomeSpecialistController extends Controller
             'price' => $request->price,
             'status' => $request->status,
         ]);
-        return Response::json(array(
+
+        return Response::json([
             'status' => 200,
             'message' => 'created successfully',
-
-        ));
+        ]);
     }
+
     public function get_all_schadule_orders()
     {
-        $data = Order::with(['user', 'coupon'])->where('specialist_id', Auth::guard('specialist-api')->user()->id)->where('status', 'schadule')->get();
-        return Response::json(array(
+        $data = Order::with(['user'])->where('specialist_id', Auth::guard('specialist-api')->user()->id)->where('status', 'schedule')->get();
+
+        $data->transform(function ($order) {
+            if ($order->user) {
+                $order->user->image_url = asset('images/users/' . $order->user->image);
+            }
+        });
+
+        return Response::json([
             'status' => 200,
             'data' => $data,
-
-
-        ));
+        ]);
     }
+
     public function get_all_finished_orders()
     {
-        $data = Order::with(['user', 'coupon'])->where('specialist_id', Auth::guard('specialist-api')->user()->id)->where('status', 'finished')->get();
-        return Response::json(array(
+        $data = Order::with(['user'])->where('specialist_id', Auth::guard('specialist-api')->user()->id)->where('status', 'finished')->get();
+
+        $data->transform(function ($order) {
+            if ($order->user) {
+                $order->user->image_url = asset('images/users/' . $order->user->image);
+            }
+        });
+
+        return Response::json([
             'status' => 200,
             'data' => $data,
-
-
-        ));
+        ]);
     }
+
     public function get_all_cancelled_orders()
     {
-        $data = Order::with(['user', 'coupon'])->where('specialist_id', Auth::guard('specialist-api')->user()->id)->where('status', 'cancelled')->get();
-        return Response::json(array(
+        $data = Order::with(['user'])->where('specialist_id', Auth::guard('specialist-api')->user()->id)->where('status', 'cancelled')->get();
+
+        $data->transform(function ($order) {
+            if ($order->user) {
+                $order->user->image_url = asset('images/users/' . $order->user->image);
+            }
+        });
+
+        return Response::json([
             'status' => 200,
             'data' => $data,
-
-
-        ));
+        ]);
     }
+
     public function get_all_pending_service_orders()
     {
-        $data = OrderService::with(['user', 'coupon'])->where('specialist_id', Auth::guard('specialist-api')->user()->id)->where('status', 'pending')->get();
-        return Response::json(array(
+        $data = OrderService::with(['user', 'orderfiles'])->where('specialist_id', Auth::guard('specialist-api')->user()->id)->where('status', 'pending')->get();
+
+        $data->transform(function ($order) {
+            $order->audio_path_url = asset('uploads/files/' . $order->audio_path);
+            if ($order->specialist) {
+                $order->specialist->image_url = asset('specialist_images/' . $order->specialist->image);
+            }
+            $order->orderfiles->transform(function ($file) {
+                if ($file->file_path) {
+                    $file->file_url = asset('uploads/files/' . $file->file_path);
+                }
+                return $file;
+            });
+            if ($order->user) {
+                $order->user->image_url = asset('images/users/' . $order->user->image);
+            }
+            return $order;
+        });
+
+        return Response::json([
             'status' => 200,
             'data' => $data,
-        ));
+        ]);
     }
 
     public function get_all_finished_service_orders()
     {
-        $data = OrderService::with(['user', 'coupon'])->where('specialist_id', Auth::guard('specialist-api')->user()->id)->where('status', 'finished')->get();
-        return Response::json(array(
+        $data = OrderService::with(['user'])->where('specialist_id', Auth::guard('specialist-api')->user()->id)->where('status', 'finished')->get();
+
+        $data->transform(function ($order) {
+            if ($order->user) {
+                $order->user->image_url = asset('images/users/' . $order->user->image);
+            }
+        });
+
+        return Response::json([
             'status' => 200,
             'data' => $data,
-
-
-        ));
+        ]);
     }
+
     public function get_all_cancelled_service_orders()
     {
-        $data = OrderService::with(['user', 'coupon'])->where('specialist_id', Auth::guard('specialist-api')->user()->id)->where('status', 'cancelled')->get();
-        return Response::json(array(
+        $data = OrderService::with(['user'])->where('specialist_id', Auth::guard('specialist-api')->user()->id)->where('status', 'cancelled')->get();
+
+        $data->transform(function ($order) {
+            if ($order->user) {
+                $order->user->image_url = asset('images/users/' . $order->user->image);
+            }
+        });
+
+        return Response::json([
             'status' => 200,
             'data' => $data,
-
-
-        ));
+        ]);
     }
+
     public function get_all_finished_normal_orders()
     {
         $data = OrderNormalSpecialist::where('specialist_id', Auth::guard('specialist-api')->user()->id)->get();
-        $normal_order = OrderNormal::with(['user', 'coupon'])->whereIn('id', $data->pluck('order_id'))->where('status', 'finished')->get();
-        return Response::json(array(
+        $normal_order = OrderNormal::with(['user'])->whereIn('id', $data->pluck('order_id'))->where('status', 'finished')->get();
+
+        $normal_order->transform(function ($order) {
+            if ($order->user) {
+                $order->user->image_url = asset('images/users/' . $order->user->image);
+            }
+            $order->audio_path_url = asset('uploads/files/' . $order->audio_path);
+            $order->orderfiles->transform(function ($file) {
+                if ($file->file_path) {
+                    $file->file_url = asset('uploads/files/' . $file->file_path);
+                }
+                return $file;
+            });
+            return $order;
+        });
+
+        return Response::json([
             'status' => 200,
             'data' => $normal_order,
-
-
-        ));
+        ]);
     }
+
     public function get_all_cancelled_normal_orders()
     {
         $data = OrderNormalSpecialist::where('specialist_id', Auth::guard('specialist-api')->user()->id)->get();
-        $normal_order = OrderNormal::with(['user', 'coupon'])->whereIn('id', $data->pluck('order_id'))->where('status', 'cancelled')->get();
-        return Response::json(array(
+        $normal_order = OrderNormal::with(['user'])->whereIn('id', $data->pluck('order_id'))->where('status', 'cancelled')->get();
+
+        $normal_order->transform(function ($order) {
+            if ($order->user) {
+                $order->user->image_url = asset('images/users/' . $order->user->image);
+            }
+            $order->audio_path_url = asset('uploads/files/' . $order->audio_path);
+            $order->orderfiles->transform(function ($file) {
+                if ($file->file_path) {
+                    $file->file_url = asset('uploads/files/' . $file->file_path);
+                }
+                return $file;
+            });
+            return $order;
+        });
+
+        return Response::json([
             'status' => 200,
             'data' => $normal_order,
-
-
-        ));
+        ]);
     }
 }
